e42aedd48b41dc3db13522932ec6cb944af11a0d
nicolasfederico <nicolasfmertens@gmail.com>
2022-03-20 22:19:52 -0300

    update

--- js/logica.js
+++ js/logica.js
@@ -1,7 +1,10 @@
 let tablaDatosGenerales = document.querySelector('#tablaDatosGenerales');
 let tablaDatosRx = document.querySelector('#tablaDatosRx');
 let tablaDatosTx = document.querySelector('#tablaDatosTx');
+let contenedorDatos = document.querySelector('#contenedorDatos');
 let datoMedidorActual =1;
+let cadenaUnida=['-1','-1','-1','-1','-1'];
+
 
 
 
@@ -14,7 +17,17 @@ const compararDatos = async () => {
             let datosMedidor = await response.json();
             datoMedidorComparar = datosMedidor.data;
             if (datoMedidorActual != datoMedidorComparar){
-                cargarDatos();
+                let dataTranslated = window.atob(datoMedidorComparar);
+                unificacionDeCadena(dataTranslated);
+                console.log('Cadena Unida:\r\n'+cadenaUnida)
+                
+                let incluyeValor = cadenaUnida.includes('-1')
+
+                if (!incluyeValor){
+                    cargarDatos(cadenaUnida);
+                }
+            
+  
                 datoMedidorActual = datoMedidorComparar;
             }
             
@@ -30,13 +43,11 @@ const compararDatos = async () => {
 
 
 
-const cargarDatos = async () =>{
+const cargarDatos = async (cadenaUnida) =>{
     try{
         let response = await fetch('http://calefaccionredimec.ddns.net:1888/medidor');
-        console.log(response);
         if(response.ok){
             let datosMedidor= await response.json();
-            console.log(datosMedidor);
             let datos;
             let datosRx;
             let datosTx;
@@ -44,7 +55,7 @@ const cargarDatos = async () =>{
             `<tr>
                 <td>${datosMedidor.applicationID}</td>
                 <td>${datosMedidor.applicationName}</td>
-                <td>${window.atob(datosMedidor.data)}</td>
+                <td>${cadenaUnida}</td>
                 <td>${datosMedidor.devEUI}</td>
                 <td>${datosMedidor.deviceName}</td>
                 <td>${datosMedidor.fCnt}</td>
@@ -75,7 +86,8 @@ const cargarDatos = async () =>{
 
             tablaDatosGenerales.innerHTML += datos;
             tablaDatosRx.innerHTML += datosRx;
-            tablaDatosTx.innerHTML += datosTx;     
+            tablaDatosTx.innerHTML += datosTx; 
+            contenedorDatos.innerHTML = cadenaUnida;
         }
         else{
             tablaDatosGenerales.innerHTML="<h1>Error - Failed URL!</h1>";
@@ -90,3 +102,13 @@ const cargarDatos = async () =>{
 setInterval(() => {
     compararDatos();
 }, 1000);
+
+
+const unificacionDeCadena = (cadena) => {
+    if (cadena.charAt(10) =='0') {
+        cadenaUnida = ['-1','-1','-1','-1','-1'];
+        cadenaUnida[cadena.charAt(10)] = cadena;
+    } else if (cadenaUnida[0]!='-1' && (cadenaUnida[0].substring(1,9))==cadena.substring(1,9)) {
+        cadenaUnida[cadena.charAt(10)] = cadena.substring(12);
+    } 
+}
